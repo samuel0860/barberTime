3749104d886d82b8a159a6b58fef061e0b05d7f8
Guilhermy Deveza <guilhermydeveza33@gmail.com>
2025-12-16 00:21:55 -0300

    Update login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://barbertime-api.onrender.com/';
 
 document.addEventListener('DOMContentLoaded', () => {
   const emailInput = document.getElementById('email');
